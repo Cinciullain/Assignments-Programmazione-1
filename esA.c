09fa5d7321ceaa58b2c40826f7f0130cc250f1ca
Cinciullain <21225666+Cinciullain@users.noreply.github.com>
2019-05-25 23:21:43 +0200

    Add files via upload

--- esA.c
+++ esA.c
@@ -1,11 +1,12 @@
 #include <stdio.h>  
+#include <stdlib.h>
 
 #define NUMERO_FUNZIONI 5
 
 void nuovoNumero(int *numero);
-char *stampaRappresentazioneC2(int numero);
-void calcolaOpposto(int numero);
-char *stampaRappresentazioneC2Opposto(char *rappresentazioneC2);
+void stampaRappresentazioneC2(int *numero);
+void calcolaOpposto(int *numero);
+void stampaRappresentazioneC2Opposto(int *numero);
 void esci(int *numero);
 
 
@@ -16,68 +17,47 @@ int main(){
     printf("Inserire un numero intero:\n");
     nuovoNumero(&numeroScelto);
     
-    int(*f[NUMERO_FUNZIONI]) (int) = {nuovoNumero, stampaRappresentazioneC2, calcolaOpposto, stampaRappresentazioneC2Opposto, esci};
+    void (*puntatore_funzioni[NUMERO_FUNZIONI])(int*) = {nuovoNumero, stampaRappresentazioneC2, calcolaOpposto, stampaRappresentazioneC2Opposto, esci};
                                   
     while(!exitProgramma){
-        printf("Scegliere un\'opzione (1 - Nuovo numero; 2 - Stampa rappresentazione C2; 3 - Calcola opposto; 4 - Stampa rappresentazione C2 dell\'opposto; 5 - Esci):")
+        printf("Scegliere un\'opzione (1 - Nuovo numero; 2 - Stampa rappresentazione C2; 3 - Calcola opposto; 4 - Stampa rappresentazione C2 dell\'opposto; 5 - Esci):");
         nuovoNumero(&funzioneScelta);
-        switch (funzioneScelta){
-            case 1: 
-                printf("Inserisci un numero intero:\n");
-                nuovoNumero(&numeroScelto);
-                break;
-            case 2:
-                printf("%s\n", stampaRappresentazioneC2(numeroScelto));
-                break;
-            case 3:
-                calcolaOpposto(numeroScelto);
-                break;
-            case 4:
-                printf("%s", stampaRappresentazioneC2Opposto(stampaRappresentazioneC2(numeroScelto)));
-                break;
-            case 5:
-                esci();
-            default:
-                break;
-        }
+        if(funzioneScelta >= 1 && funzioneScelta <= 5)
+            puntatore_funzioni[funzioneScelta - 1];
     }
     return 0;
 }
 
 void nuovoNumero(int *numero){
-    //printf("Inserisci un numero intero:\n");
     while(!scanf("%d", numero)){
         printf("Errore. Inserisci un numero intero.\n");
         scanf("%*[^\n]%*c");
     }
 }
 
-void stampaRappresentazioneC2(int numero){
+void stampaRappresentazioneC2(int *numero){
 
     char *rappresentazioneC2;
     rappresentazioneC2 = malloc(sizeof(char)*36 + 1);
     rappresentazioneC2[37] = '\0';
     int i = 36;
-
-    while(){
-        
-    }
-
-
 }
 
-void calcolaOpposto(int numero){
-    printf("%d\n", -numero);
+void calcolaOpposto(int *numero){
+    printf("%d\n", - *numero);
 }
 
-void stampaRappresentazioneC2Opposto(char *rappresentazioneC2){
+void stampaRappresentazioneC2Opposto(int *numero){
     int i = 0;
+    char *rappresentazioneC2;
+    rappresentazioneC2 = malloc(sizeof(char)*36 + 1);
+    rappresentazioneC2[37] = '\0';
 
-    while(rappresentazioneC2(i) != '\0'){
-        if(rappresentazioneC2(i) == '0')
-            rappresentazioneC2(i) = '1';
+    while(rappresentazioneC2[i] != '\0'){
+        if(rappresentazioneC2[i] == '0')
+            rappresentazioneC2[i] = '1';
         else
-            rappresentazioneC2(i) = '0';
+            rappresentazioneC2[i] = '0';
         
         i++;
     }
@@ -88,5 +68,5 @@ void stampaRappresentazioneC2Opposto(char *rappresentazioneC2){
 
 void esci(int *numero){
     printf("Bye bye!\n");
-    numero = 1;
+    *numero = 1;
 }
